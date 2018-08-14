8be48501ae1192d1a5abb2683f0c13667db24719
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-08-14 15:13:26 +0700

    update

---
+++ LessonX/MyProject/MyProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyProject
+//
+//  Created by Nguyen Duc Hoang on 8/14/18.
+//  Copyright © 2018 Nguyen Duc Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
